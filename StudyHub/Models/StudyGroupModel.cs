b1f4cefc50667f0bcee3c929840882bd9d30f7cd
viniraf <vrmb.pro@gmail.com>
2023-12-06 22:27:39 -0300

    Created initials models

---
+++ StudyHub/Models/StudyGroupModel.cs
@@ -0,0 +1,19 @@
+﻿namespace StudyHub.Models
+{
+    public class StudyGroupModel
+    {
+        public int Id { get; set; }
+
+        public int CreateUserId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+
+        public String Title { get; set; } = "";
+
+        public String Description { get; set; } = "";
+
+        public String Link { get; set; } = ""; 
+    }
+}
---
+++ StudyHub/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿namespace StudyHub.Models;
+
+public class UserModel
+{
+    public int Id { get; set; }
+
+    public DateTime CreateDate { get; set; }
+
+    public DateTime UpdateDate { get; set; }
+
+    public String Name { get; set; } = "";
+
+    public String Profile { get; set; } = "";
+
+    public String Username { get; set; } = "";
+
+    public String Password { get; set; } = "";
+
+}
